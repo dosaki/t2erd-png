fccec2e805feb0135ab49456e0b67de359874d18
Tiago Correia <tiago.f.a.correia@gmail.com>
2018-08-10 17:10:49 +0100

    Split from t2erd to isolate the png creation bit.

--- src/t2erd.js
+++ src/t2erd.js
@@ -4,13 +4,11 @@
 
 // Text to ERD
 const minimist = require('minimist');
-const Parser = require("./parser/parser.js");
-const Diagram = require("./drawer/diagram.js");
+const t2erd = require('t2erd');
 const svgToImg = require("svg-to-img");
 const fs = require('fs');
 
 const argv = minimist(process.argv.slice(2));
-
 const input = argv.i;
 const output = argv.o;
 
@@ -18,8 +16,7 @@ if(!!input){
   fs.readFile(input, 'utf8', (err, data) => {
     if (err) throw err;
 
-    const diagram = new Diagram(new Parser(data), {});
-    const svgCode = diagram.toString();
+    const svgCode = t2erd(data);
     console.log(svgCode);
     (async () => {
       const image = await svgToImg.from(svgCode).toPng({
@@ -27,8 +24,4 @@ if(!!input){
       });
     })();
   });
-}
-
-module.exports = (inputText, params) => {
-  return new Diagram(new Parser(inputText), (!!params ? params : {})).toString();
 }
